543679f517ff7356005c58c6ea231bfdd3ef011d
Ojasvi-74 <ojasviverma174@gmail.com>
2024-12-14 23:30:33 +0530

    Initial commit

---
+++ admin_login.php
@@ -0,0 +1,90 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>LMS | Login</title>
+    <meta charset="utf-8" name="viewport" content="width=device-width,intial-scale=1">
+    <link rel="stylesheet" type="text/css" href="../bootstrap-4.4.1/css/bootstrap.min.css">
+      <script type="text/javascript" src="./bootstrap-4.4.1/js/juqery_latest.js"></script>
+      <script type="text/javascript" src="./bootstrap-4.4.1/js/bootstrap.min.js"></script>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="index.php">Library Management System</a>
+            </div>
+            <ul class="nav navbar-nav navbar-right">
+                <li class="nav-item">
+                  <a class="nav-link" href="./index.php">User Login</a>
+                </li>
+              <li class="nav-item">
+                <a class="nav-link" href="./admin_login.php">Admin Login</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="../signup.php"></span>Signup</a>
+              </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="row">
+        <div class="col-md-4" id="side_bar">
+            <h5>Today's Quote</h5>
+            <h6>“There is more treasure in books than in all the pirate's loot on Treasure Island"</h6>
+            <p>~ Walt Disney</p>
+            <h5>Library Timing</h5>
+            <ul>
+                <li>Opening: 9:00 AM</li>
+                <li>Closing: 6:00 PM</li>
+            </ul>
+            <h5>What We provide ?</h5>
+            <ul>
+                <li>AC Rooms</li>
+                <li>Free Wi-fi</li>
+                <li>Learning Environment</li>
+                <li>Discussion Room</li>
+                <li>Free Electricity</li>
+            </ul>
+        </div>
+        <div class="col-md-8" id="main_content">
+            <center><h3><u>Admin Login Form</u></h3></center>
+            <form action="" method="post">
+                <div class="form-group">
+                    <label for="email">Email ID:</label>
+                    <input type="text" name="email" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label for="password">Password:</label>
+                    <input type="password" name="password" class="form-control" required>
+                </div>
+                <button type="submit" name="login" class="btn btn-primary">Login</button>    
+            </form>
+            <?php 
+                if(isset($_POST['login'])){
+                    $connection = mysqli_connect("localhost","root","");
+                    $db = mysqli_select_db($connection,"lms");
+                    $query = "select * from admins where email = '$_POST[email]'";
+                    $query_run = mysqli_query($connection,$query);
+                    while ($row = mysqli_fetch_assoc($query_run)) {
+                        if($row['email'] == $_POST['email']){
+                            if($row['password'] == $_POST['password']){
+                                $_SESSION['name'] =  $row['name'];
+                                $_SESSION['email'] =  $row['email'];
+                                header("Location: admin_dashboard.php");
+                            }
+                            else{
+                                ?>
+                                <br><br><center><span class="alert-danger">Wrong Password !!</span></center>
+                                <?php
+                            }
+                        }
+                    }
+                }
+            ?>
+        </div>
+    </div>
+</body>
+</html>
